c136b907c42ad378d4989435e62a665b2ad5ad51
matei jordache <mateijordache@live.com>
2023-06-30 00:42:13 -0400

    start working on firmware

---
+++ firmware/src/aioli-board.h
@@ -0,0 +1,38 @@
+
+#pragma once
+
+// MOTOR DRIVER
+#define U_PWM
+#define U_EN
+#define V_PWM
+#define V_EN
+#define W_PWM
+#define W_EN
+
+// MOTOR ENCODER
+#define ENC_CIPO
+#define ENC_SCK
+#define ENC_CS
+
+// CANBUS
+#define CAN_TX
+#define CAN_RX
+
+// GC9AO1 TFT DISPLAY
+#define TFT_COPI
+#define TFT_SCK
+#define TFT_CS
+#define TFT_DC
+#define TFT_BL
+
+// GENERAL UART
+#define UART2_TX
+#define UART2_RX
+
+// GENERAL I2C
+#define I2C1_SDA
+#define I2C1_SCL
+
+//MISC
+#define USER_LED 
+#define DFU_PIN
---
+++ firmware/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <SimpleFOC.h>
+#include <SimpleFOCDrivers.h>
+
+#include "./stspin32g4_hw.h"
+#include "./funqi_hw.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
