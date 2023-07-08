9c6472a44696d8d832de199c15b727ae2191bb63
matei jordache <mateijordache@live.com>
2023-07-08 19:32:21 -0400

    Base firmware working

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -6,21 +6,22 @@
 
 #include "./drv_reset.h"
 #include "./aioli-board.h"
-// #include "./can.c"
+// #include "./can.cpp"
 
 // Motor specific parameters.
-#define POLEPAIRS 6
+#define POLEPAIRS 4
 #define RPHASE 1.4
 #define MOTORKV 1000
 
+uint8_t useDFU = 0;
 uint8_t pendingFrame = 0;
 uint8_t sfocCmdStr;
 
 // simpleFOC constructors
 BLDCDriver3PWM driver = BLDCDriver3PWM(U_PWM, V_PWM, W_PWM, U_EN, V_EN, W_EN);
 BLDCMotor motor = BLDCMotor(POLEPAIRS, RPHASE, MOTORKV);
-MagneticSensorMT6701SSI enc(ENC_CS);
-Commander commander = Commander(Serial);
+MagneticSensorMT6701SSI enc = MagneticSensorMT6701SSI(ENC_CS);
+Commander commander = Commander(SerialUSB);
 
 uint8_t configureFOC(void);
 uint8_t configureCAN(void);
@@ -33,6 +34,9 @@ void doMotor(char *cmd)
 
 void setup()
 {
+	pinMode(USER_LED, OUTPUT);
+	pinMode(USER_BUTTON, INPUT);
+
 	SerialUSB.begin();
 
 	Serial.println(configureFOC() == 1 ? "SFOC successfully init." : "SFOC failed to init.");
@@ -44,16 +48,17 @@ void loop()
 {
 	motor.loopFOC();
 	motor.move();
+	commander.run();
 
-	if(pendingFrame){
-		commander.run((char*)sfocCmdStr);
-		pendingFrame = 0;
-	}
-	else{
-		commander.run();
-	}
+	// if(pendingFrame){
+	// 	commander.run((char*)sfocCmdStr);
+	// 	pendingFrame = 0;
+	// }
+	// else{
+	// 	commander.run();
+	// }
 
-	#ifdef HAS_COMMANDER
+	#ifdef HAS_MONITOR
 	motor.monitor();
 	#endif
 }
@@ -69,32 +74,13 @@ uint8_t configureFOC(){
 	// Encoder initialization.
 	// Encoder on SPI1
 	enc.init();
-	motor.linkSensor(&enc);
-	// Encoder initialization.
-	// Encoder on SPI1
-	enc.init();
-	motor.linkSensor(&enc);
 
 	// Driver initialization.
-	driver.pwm_frequency = 20000;
+	driver.pwm_frequency = 32000;
 	driver.voltage_power_supply = 5;
-	driver.voltage_limit = 3;
+	driver.voltage_limit = 5;
 	driver.init();
-	motor.linkDriver(&driver);
-	// Driver initialization.
-	driver.pwm_frequency = 20000;
-	driver.voltage_power_supply = 5;
-	driver.voltage_limit = 3;
-	driver.init();
-	motor.linkDriver(&driver);
 
-	// Motor PID parameters.
-	motor.PID_velocity.P = 0.2;
-	motor.PID_velocity.I = 3;
-	motor.PID_velocity.D = 0.002;
-	motor.PID_velocity.output_ramp = 100;
-	motor.LPF_velocity.Tf = 0.5;
-	motor.LPF_angle.Tf = 0; // try to avoid
 	// Motor PID parameters.
 	motor.PID_velocity.P = 0.2;
 	motor.PID_velocity.I = 3;
@@ -107,13 +93,7 @@ uint8_t configureFOC(){
 	motor.voltage_sensor_align = 2;
 	motor.current_limit = 0.5;
 	motor.velocity_limit = 20;
-	motor.controller = MotionControlType::angle;
-	motor.foc_modulation = FOCModulationType::SinePWM;
-	// Motor initialization.
-	motor.voltage_sensor_align = 2;
-	motor.current_limit = 0.5;
-	motor.velocity_limit = 20;
-	motor.controller = MotionControlType::angle;
+	motor.controller = MotionControlType::velocity;
 	motor.foc_modulation = FOCModulationType::SinePWM;
 
 	// Monitor initialization
@@ -121,17 +101,23 @@ uint8_t configureFOC(){
 	motor.useMonitoring(Serial);
 	motor.monitor_start_char = 'M';
 	motor.monitor_end_char = 'M';
-	motor.monitor_downsample = 500;
+	motor.monitor_downsample = 250;
 	#endif
 
+	motor.linkSensor(&enc);
+	motor.linkDriver(&driver);
 	motor.init();
+
+	motor.target = 20;
+
 	return motor.initFOC();
 }
 
 uint8_t configureCAN(){
-
+	return 0;
 }
 
 uint8_t configureDFU(){
-
+	
+	return 1;
 }
