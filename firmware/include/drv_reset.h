83fbc7f4673eb8896f94a05a1b4ad434398f58c8
Graham Forest <vitaminmoo@gmail.com>
2024-05-23 15:21:04 -0700

    Add a bunch of linting

--- firmware/include/drv_reset.h
+++ firmware/include/drv_reset.h
@@ -1,4 +1,5 @@
+// Copyright 2024 matei jordache <mateijordache@live.com>
 #pragma once
 
-void perform_system_reset( void );
-void jump_to_bootloader( void );
+void perform_system_reset(void);
+void jump_to_bootloader(void);
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,11 +1,12 @@
+// Copyright 2024 matei jordache <mateijordache@live.com>
 #include <Arduino.h>
 #include <SPI.h>
 #include <SimpleFOC.h>
 #include <SimpleFOCDrivers.h>
-#include "encoders/mt6701/MagneticSensorMT6701SSI.h"
 
-#include "./drv_reset.h"
 #include "./aioli-board.h"
+#include "./drv_reset.h"
+#include "encoders/mt6701/MagneticSensorMT6701SSI.h"
 // #include "./can.cpp"
 
 // Motor specific parameters.
@@ -27,97 +28,90 @@ uint8_t configureFOC(void);
 uint8_t configureCAN(void);
 uint8_t configureDFU(void);
 
-void doMotor(char *cmd)
-{
-	commander.motor(&motor, cmd);
+void doMotor(char *cmd) { commander.motor(&motor, cmd); }
+
+void setup() {
+  pinMode(USER_LED, OUTPUT);
+  pinMode(USER_BUTTON, INPUT);
+
+  SerialUSB.begin();
+
+  Serial.println(configureFOC() == 1 ? "SFOC successfully init."
+                                     : "SFOC failed to init.");
+  Serial.println(configureCAN() == 1 ? "CAN successfully init."
+                                     : "CAN failed to init.");
+  Serial.println(configureDFU() == 1 ? "DFU successfully init."
+                                     : "DFU failed to init.");
 }
 
-void setup()
-{
-	pinMode(USER_LED, OUTPUT);
-	pinMode(USER_BUTTON, INPUT);
+void loop() {
+  motor.loopFOC();
+  motor.move();
+  commander.run();
 
-	SerialUSB.begin();
+  // if(pendingFrame){
+  //   commander.run((char*)sfocCmdStr);
+  //   pendingFrame = 0;
+  // }
+  // else{
+  //   commander.run();
+  // }
 
-	Serial.println(configureFOC() == 1 ? "SFOC successfully init." : "SFOC failed to init.");
-	Serial.println(configureCAN() == 1 ? "CAN successfully init."  : "CAN failed to init.");
-	Serial.println(configureDFU() == 1 ? "DFU successfully init."  : "DFU failed to init.");
+#ifdef HAS_MONITOR
+  motor.monitor();
+#endif
 }
 
-void loop()
-{
-	motor.loopFOC();
-	motor.move();
-	commander.run();
+uint8_t configureFOC() {
+  commander.add('M', doMotor, "motor");
+  commander.verbose = VerboseMode::machine_readable;
 
-	// if(pendingFrame){
-	// 	commander.run((char*)sfocCmdStr);
-	// 	pendingFrame = 0;
-	// }
-	// else{
-	// 	commander.run();
-	// }
+#ifdef SIMPLEFOC_STM_DEBUG
+  SimpleFOCDebug::enable(&Serial);
+#endif
 
-	#ifdef HAS_MONITOR
-	motor.monitor();
-	#endif
+  // Encoder initialization.
+  // Encoder on SPI1
+  enc.init();
+
+  // Driver initialization.
+  driver.pwm_frequency = 32000;
+  driver.voltage_power_supply = 5;
+  driver.voltage_limit = 5;
+  driver.init();
+
+  // Motor PID parameters.
+  motor.PID_velocity.P = 0.2;
+  motor.PID_velocity.I = 3;
+  motor.PID_velocity.D = 0.002;
+  motor.PID_velocity.output_ramp = 100;
+  motor.LPF_velocity.Tf = 0.5;
+  motor.LPF_angle.Tf = 0;  // try to avoid
+
+  // Motor initialization.
+  motor.voltage_sensor_align = 2;
+  motor.current_limit = 0.5;
+  motor.velocity_limit = 20;
+  motor.controller = MotionControlType::velocity;
+  motor.foc_modulation = FOCModulationType::SinePWM;
+
+// Monitor initialization
+#ifdef HAS_MONITOR
+  motor.useMonitoring(Serial);
+  motor.monitor_start_char = 'M';
+  motor.monitor_end_char = 'M';
+  motor.monitor_downsample = 250;
+#endif
+
+  motor.linkSensor(&enc);
+  motor.linkDriver(&driver);
+  motor.init();
+
+  motor.target = 20;
+
+  return motor.initFOC();
 }
 
-uint8_t configureFOC(){
-	commander.add('M', doMotor, "motor");
-	commander.verbose = VerboseMode::machine_readable;
+uint8_t configureCAN() { return 0; }
 
-	#ifdef SIMPLEFOC_STM_DEBUG
-	SimpleFOCDebug::enable(&Serial);
-	#endif
-
-	// Encoder initialization.
-	// Encoder on SPI1
-	enc.init();
-
-	// Driver initialization.
-	driver.pwm_frequency = 32000;
-	driver.voltage_power_supply = 5;
-	driver.voltage_limit = 5;
-	driver.init();
-
-	// Motor PID parameters.
-	motor.PID_velocity.P = 0.2;
-	motor.PID_velocity.I = 3;
-	motor.PID_velocity.D = 0.002;
-	motor.PID_velocity.output_ramp = 100;
-	motor.LPF_velocity.Tf = 0.5;
-	motor.LPF_angle.Tf = 0; // try to avoid
-
-	// Motor initialization.
-	motor.voltage_sensor_align = 2;
-	motor.current_limit = 0.5;
-	motor.velocity_limit = 20;
-	motor.controller = MotionControlType::velocity;
-	motor.foc_modulation = FOCModulationType::SinePWM;
-
-	// Monitor initialization
-	#ifdef HAS_MONITOR
-	motor.useMonitoring(Serial);
-	motor.monitor_start_char = 'M';
-	motor.monitor_end_char = 'M';
-	motor.monitor_downsample = 250;
-	#endif
-
-	motor.linkSensor(&enc);
-	motor.linkDriver(&driver);
-	motor.init();
-
-	motor.target = 20;
-
-	return motor.initFOC();
-}
-
-uint8_t configureCAN(){
-	return 0;
-}
-
-uint8_t configureDFU(){
-
-	return 1;
-}
+uint8_t configureDFU() { return 1; }
